eab5ced716052dcb16a1415f261af645ca251852
Mihir Panjikar <mihirpanjikar@gmail.com>
2023-09-15 08:10:48 -0700

    Added logger and status codes

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI, Form, Request
+import uvicorn
+from fastapi import FastAPI, Form, HTTPException, Request, status
 from fastapi.templating import Jinja2Templates
 import smtplib
 from email.message import EmailMessage
 import os
+import logging
+
+logging.basicConfig(filename='example.log', format= '%(asctime)s %(levelname)s:%(message)s', filemode='w', encoding='utf-8', level=logging.DEBUG)
 
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
@@ -23,5 +27,9 @@ def submit(Email: str = Form(), Subject: str = Form(), Message: str = Form()):
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(str(os.environ.get('EMAIL_USER')), str(os.environ.get('EMAIL_PASS')))
         smtp.send_message(msg)
+        
+    logging.info(f"status code: {status.HTTP_200_OK}, detail: Email successfully sent")
+    return HTTPException(status_code=status.HTTP_200_OK, detail="Email successfully sent")
 
-    return "email successfully sent"
+if __name__ == "__main__":
+    uvicorn.run(app)
